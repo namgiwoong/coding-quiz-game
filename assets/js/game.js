5fe2b957f00b25a7503a011f4e237dad1f32c2c1
name <nameric@ymail.com>
2022-06-21 19:58:24 -0700

    commenting in game.js on what needs to be done.

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+// Declare "count down" - numbers
+
+// declare "questions"
+// var questions = [
+//question
+//answers
+//correct answer},{
+//question
+//answers
+//correct answer
+//}];
+
+//Declare the "timeEl"
+// html - <div> Timer: <span> id="timer" </span> ></div>
+// var timeEl = document.getElementById ("timer")
