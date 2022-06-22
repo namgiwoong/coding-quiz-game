17675461283ddd6b82332d53a6759ac53054850d
name <nameric@ymail.com>
2022-06-21 20:11:03 -0700

    continued adding comments in game.js on work needed to be done.

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,4 +12,18 @@
 
 //Declare the "timeEl"
 // html - <div> Timer: <span> id="timer" </span> ></div>
-// var timeEl = document.getElementById ("timer")
+// var timeEl = document.querySelector ("#timer")
+
+//Function "startGame"
+// hide the start screen
+// display the first question
+// set the starting value of "countdown"
+// start the timer "startTimer"
+
+// Function "answerQuestion"
+// check if the selected answer is correct
+// IF the answer is wrong
+// THEN we need to subtract from "countdown"
+
+//Display the next question
+//Function "displayCurrentQuestion"- ref- web apis day 2
