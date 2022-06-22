8efeee493f2e500ee1f1f176633031e998ecb651
name <nameric@ymail.com>
2022-06-21 21:21:53 -0700

    adding more comments in game.js.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,29 +1,52 @@
-// Declare "count down" - numbers
+// Declare "count down"  numbers
 
 // declare "questions"
+// var questionPosition = 0;
 // var questions = [
-//question
+//{ question
+//answers- answer: ["answer A", "answer B",]
+//correct answer},
+//{question
 //answers
-//correct answer},{
-//question
-//answers
-//correct answer
-//}];
+//correct answer }
+// ];
 
+// var currentQuestion = questions[questionPosition];
+//Declare the "timeInterval"
+// var timeInterval =
 //Declare the "timeEl"
+// var currentQuestion = questions[questionPosition];
 // html - <div> Timer: <span> id="timer" </span> ></div>
 // var timeEl = document.querySelector ("#timer")
 
 //Function "startGame"
 // hide the start screen
-// display the first question
+// set the question position to 0
+// display the current question
 // set the starting value of "countdown"
 // start the timer "startTimer"
+// IF "countdown" === 0 THEN "endGame()"
 
 // Function "answerQuestion"
+// var currentQuestion = questions[questionPosition];
 // check if the selected answer is correct
 // IF the answer is wrong
 // THEN we need to subtract from "countdown"
 
 //Display the next question
+// questionPosition++;
+// displayCurrentQuestion();
+//IF I've passed the last question THEN "endGame() ELSE displayCurrentQuestion()"
+
 //Function "displayCurrentQuestion"- ref- web apis day 2
+// var currentQuestion = questions[questionPosition];
+
+//Function "endGame"
+// function endGame(){
+//     clearInterval(timeInterval);
+// }
+//hide question area
+//show the record high score
+//clear time interval to stop it from running
+
+//Function "recordTheHighscore"- ref-web api day 3
