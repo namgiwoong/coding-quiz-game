58e86accfd01eb3c141cc5997f5f27a5933ff8b7
name <nameric@ymail.com>
2022-07-05 20:25:16 -0700

    console logged start to make sure game starts.

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,30 +3,37 @@
 // declare "questions"
 var questionPosition = 0;
 var questions = [
-{ question: ["Question 1"]
-    answers: ["answer A", "answer B","answer C", "answer D"]
-correct answer: ["answer A"]},
-{question: ["Question 2"]
-answers: ["answer A", "answer B","answer C", "answer D"]
-correct answer: ["answer B"]} 
+  {
+    question: "Question 1",
+    answers: ["answer A", "answer B", "answer C", "answer D"],
+    correctAnswer: "answer A",
+  },
+  {
+    question: "Question 2",
+    answers: ["answer A", "answer B", "answer C", "answer D"],
+    correctAnswer: "answer B",
+  },
 ];
 
-// var currentQuestion = questions[questionPosition];
+var currentQuestion = questions[questionPosition];
 //Declare the "timeInterval"
-// var timeInterval =
 //Declare the "timeEl"
-// var currentQuestion = questions[questionPosition];
-// html - <div> Timer: <span> id="timer" </span> ></div>
-// var timeEl = document.querySelector ("#timer")
+var timeEl = document.querySelector("#timer");
 
 //Function "startGame"
-// hide the start screen
-// set the question position to 0
-// display the current question
-// set the starting value of "countdown"
-// start the timer "startTimer"
-// IF "countdown" === 0 THEN "endGame()"
+function startQuiz() {
+  console.log("started");
 
+  // hide the start screen
+  // set the question position to 0
+  questionPosition = 0;
+  // display the current question
+
+  // set the starting value of "countdown"
+  // start the timer "startTimer"
+  // IF "countdown" === 0 THEN "endGame()"
+}
+document.addEventListener("click", startQuiz);
 // Function "answerQuestion"
 // var currentQuestion = questions[questionPosition];
 // check if the selected answer is correct
